7856fafe315c50411280d81b91427fbd3e7abdfb
Thomas <thomasyuan1@gmail.com>
2024-03-12 23:09:48 +0000

    fix

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ import os
 from setuptools import setup, Extension
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
+module_path = os.path.dirname(__file__)
 setup(
     name='upfirdn2d',
     ext_modules=[
         CUDAExtension('upfirdn2d', [
-            os.path.join(module_path, "upfirdn2d.cpp"),
-            os.path.join(module_path, "upfirdn2d_kernel.cu"),
+            os.path.join(module_path, "op/upfirdn2d.cpp"),
+            os.path.join(module_path, "op/upfirdn2d_kernel.cu"),
         ],
         extra_compile_args={'cxx': ['-g'], 'nvcc': ['-O2']}),
     ],
